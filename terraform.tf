e56e3a5987161754324c82025a0fda599afee32a
Stuart Tyrell <Stuart.Tyrrell@dvla.gov.uk>
2021-11-07 12:13:44 +0000

    setup tfvar file

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,4 @@
 service_name           = "sttest" #service_name can't contain punctuation marks
 service_number         = "001"
-service_location       = "Uk South"
+service_location       = "UK South"
 service_short_location = "uks"
