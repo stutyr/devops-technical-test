560e9485ed54742e53f8139e3cf1aedc490e6571
Stuart Tyrell <Stuart.Tyrrell@dvla.gov.uk>
2021-11-09 12:51:11 +0000

    rough draft of deployment steps

--- release.sh
+++ release.sh
@@ -13,8 +13,8 @@ azcopy pricing.css
 
 # Step 3 - build node.js docker image and push to registry
 cd ../app
-docker build . -t sttest.azurecr.io/merchandisesite:0.0.2
-docker push sttest.azurecr.io/merchandisesite:0.0.2
+docker build . -t xxx.azurecr.io/merchandisesite:0.0.2
+docker push xxx.azurecr.io/merchandisesite:0.0.2
 
 # Step 4 - Helm chart to install docker image into AKS
 cd ../helm
